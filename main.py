59362ffd93c6b880d9b39ba12b618daf5d8ba76d
flowy-data <gayletan@gmail.com>
2021-01-12 16:18:43 +0800

    update to call pyspark and csv load

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import get_data_with_praw
+import load_pyspark
+import clean_pyspark
 
 if __name__ == "__main__":
-    get_data_with_praw.get_data(topic="MachineLearning", limit=100, file_output="ml.csv")
+    # print("Get Data from Reddit API PRAW Package")
+    # data_dict = get_data_with_praw.get_data_as_dict(topic="MachineLearning", limit=50)
+    #save to csv
+    # print("Saving Output:")
+    # get_data_with_praw.save_csv(input_dict=data_dict, file_output="ml.csv")
+    
+    # or use pyspark
+    spark = load_pyspark.init_spark()
+    # df = load_pyspark.create_pyspark_df_from_dict(spark_session=spark, input_dict=data_dict)
+
+    #load from csv -- avoid calling APIs repeatedly
+    df = load_pyspark.create_pyspark_df_from_csv(spark_session=spark, input_file="ml.csv")
+
+    # do more operations with pyspark
+    df =clean_pyspark.change_types(df)
+    df = clean_pyspark.add_columns(df)
+    clean_pyspark.check_new_columns(df, "created_utc")
